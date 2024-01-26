cbe77c4e49ba834e4124cc5f430390809cf16790
Baksvell <Patsi2@yandex.ru>
2024-01-27 00:04:32 +0400

    homework1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("TestItem", 400, 5)
+    assert item.calculate_total_price() == 2000
+
+
+def test_apply_discount():
+    item = Item("TestItem", 400, 3)
+    Item.pay_rate = 0.5
+    item.apply_discount()
+    assert item.price == 400 * Item.pay_rate
