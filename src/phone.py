86f2755b4413884421569a717bf0276089bad158
Baksvell <Patsi2@yandex.ru>
2024-02-27 10:28:45 +0400

    fix_homework4

--- src/phone.py
+++ src/phone.py
@@ -2,24 +2,31 @@ from src.item import Item
 
 
 class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        """
+        Создание экземпляра класса Phone.
 
-    def __init__(self, name, price, quantity, number_of_sim):
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param number_of_sim: Количество поддерживаемых сим-карт
+        """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
-
-    def __str__(self):
-        return f'{self.name}'
 
     @property
     def number_of_sim(self):
         return self.__number_of_sim
 
     @number_of_sim.setter
-    def number_of_sim(self, value: int):
-        if value > 0 and isinstance(value, int):
-            self.__number_of_sim = value
+    def number_of_sim(self, testing_data):
+        if int(testing_data) > 0:
+            self.__number_of_sim = int(testing_data)
         else:
-            print('ValueError: Количество физических SIM-карт должно быть целым числом больше нуля')
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self) -> str:
+        return f'{self.name}'
